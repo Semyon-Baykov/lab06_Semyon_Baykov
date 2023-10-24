4696241fa9e9957bb85e6774b59a1fba94477d23
Simon Baykov <simon.baykov@gmail.com>
2023-10-24 14:28:17 -0400

    Added main() and encode()

---
+++ lab6_version_control.py
@@ -0,0 +1,30 @@
+# Encoder stores the encoded password to a new variable, with each digit being shifted up by 3 numbers
+def encode(pswd):
+    output = ''
+    for i in range(len(pswd)):
+        output += str(int(pswd[i]) + 3)[-1]
+    return output
+
+
+# Decoder code goes here (inverse of encode())
+def decode(pswd):
+    pass
+
+
+# Main function with menu loop
+def main():
+    while True:
+        print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
+        opt = input('Please enter an option: ')
+        if opt == '1':
+            pswd = encode(input('Please enter your password to encode: '))
+            print('Your password has been encoded and stored!\n')
+        elif opt == '2':
+            print(f'THe encoded password is {pswd}, and the original password is {decode(pswd)}.\n')
+        else:
+            return
+
+
+# Call main
+if __name__ == '__main__':
+    main()
